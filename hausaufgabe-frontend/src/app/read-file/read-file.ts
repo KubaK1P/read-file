521c4b7baa1451603931247758a424a3c8990667
KubaK1P <1p22kus@gmail.com>
2026-01-28 18:36:12 +0100

    feat(anular): add fetch (doesn't work)

--- hausaufgabe-frontend/src/app/read-file/read-file.ts
+++ hausaufgabe-frontend/src/app/read-file/read-file.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FileText } from '../file-text';
 
 @Component({
   selector: 'app-read-file',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './read-file.css',
 })
 export class ReadFile {
-
+  private http = inject(HttpClient);
+  private path = "http://localhost:5185/file";
+  public fileText: FileText = { text: "Unable to read the file bruh" };
+  constructor() {
+    this.http.get<FileText>(this.path).subscribe((res) => {
+      console.log(res);
+      this.fileText = res;
+    })
+  }
 }
